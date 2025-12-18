1ecf901c341089a5c37998b5508afb39dc0134c3
Kareem Khaled <123971567+kareemkhaledd1@users.noreply.github.com>
2025-12-18 20:25:54 +0200

    fix(notification): update import path for NotificationScheduler to follow naming conventions

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { ProductModule } from './product/product.module';
 import { StaticModule } from './static/static.module';
 import { AdminModule } from './admin/admin.module';
 import { SmsModule } from './sms/sms.module';
-import { NotificationScheduler } from 'src/notification/NotificationScheduler';
+import { NotificationScheduler } from 'src/notification/notificationScheduler';
 
 @Module({
   imports: [
