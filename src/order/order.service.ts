2bf740722ec204e5e803b0c576b439b4bcd86f37
Kareem Khaled <123971567+kareemkhaledd1@users.noreply.github.com>
2026-04-11 21:00:12 +0200

    fix(order): rename discount variable to totalDiscount for clarity in order fetching

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -717,7 +717,7 @@ export class OrderService {
           service: services,
           clientName: `${client.firstName} ${client.lastName}`,
           clientPhone: client.phone,
-          discount: order.subTotal - order.total,
+          totalDiscount: order.subTotal - order.total,
         },
         'Order fetched successfully',
         200,
