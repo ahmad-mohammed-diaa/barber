c4717b4cda1241d2a711ecd8dabd76a8e9eaa2d2
Kareem Khaled <123971567+kareemkhaledd1@users.noreply.github.com>
2026-04-27 01:32:47 +0300

    feat(auth): add transformation for vacations field to handle string input as JSON

--- src/auth/dto/auth-register-dto.ts
+++ src/auth/dto/auth-register-dto.ts
@@ -58,6 +58,16 @@ export class RegisterDto {
   @IsArray()
   @IsOptional()
   @ValidateIf((o) => ['CASHIER', 'BARBER'].includes(o?.role?.toUpperCase()))
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      try {
+        return JSON.parse(value);
+      } catch {
+        return value;
+      }
+    }
+    return value;
+  })
   vacations: Vacation[];
 
   @ValidateIf((o) => ['CASHIER', 'BARBER'].includes(o?.role?.toUpperCase()))
